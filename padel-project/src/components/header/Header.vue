072fff0eeb18327d91087c22b710f19828bd815a
oscarHDC <oscarhdec01@gmail.com>
2023-10-30 20:25:26 +0100

    Removing useless default files

---
+++ padel-project/src/components/header/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Esto es el header</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio alias quam iure nesciunt asperiores placeat facere
+    unde sit obcaecati dicta soluta provident, rem officia ullam inventore doloribus laborum sapiente consequatur!</p>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang=""></style>
